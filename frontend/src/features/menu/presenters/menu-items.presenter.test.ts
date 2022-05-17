9c71d4763bd37703bee524d28322bd487b5cd180
Alexander Zubko <alexander.zubko@gmail.com>
2022-05-17 09:22:33 +0200

    ✅ Add snapshot test for general case of products props

---
+++ frontend/src/features/menu/presenters/menu-items.presenter.test.ts
@@ -0,0 +1,45 @@
+import { Product } from "@app/core/entities/Product";
+import { setProducts } from "@app/core/repository/products.repository";
+import { resetAllStores } from "@app/core/repository/repository";
+import { first } from "rxjs";
+import { updateCurrentCategory } from "../repository/menu.repository";
+import { getCurrentItems$ } from "./menu-items.presenter";
+
+const Products: Product[] = [
+  {
+    id: "1",
+    category: "pizza",
+    name: "name-1",
+    price: 1,
+    image: "",
+  },
+  {
+    id: "2",
+    category: "pizza",
+    name: "name-2",
+    price: 2,
+    image: "",
+  },
+  {
+    id: "3",
+    category: "drink",
+    name: "drink-1",
+    price: 3,
+    image: "",
+  },
+];
+
+describe("Menu items presenter", () => {
+  afterEach(() => {
+    resetAllStores();
+  });
+
+  it("returns the view model with the products of the current category", () => {
+    updateCurrentCategory("pizza");
+    setProducts(Products);
+    getCurrentItems$
+      .pipe(first())
+      .subscribe((props) => expect(props).toMatchSnapshot())
+      .unsubscribe();
+  });
+});
